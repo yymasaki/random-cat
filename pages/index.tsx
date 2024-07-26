73a5dce79425131d5d91c0013f00662c144a3884
yymasaki <y.masaki34@icloud.com>
2024-07-27 00:08:09 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import { NextPage } from "next";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-);
+const IndexPage: NextPage = () => {
+    return <div>猫画像予定地</div>
+};
 
 export default IndexPage;
+
+type Image = {
+    url: string;
+}
+
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    console.log(images);
+    return images[0];
+};
+
+fetchImage().then((image) => {
+    console.log(image);
+})
